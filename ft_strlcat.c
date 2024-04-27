e82f2f1be7081ed44b34da4a30eef54c15917dfd
ryusukeyashiro <ryusukeyashiro@ryusuke2024.local>
2024-04-27 17:17:58 +0900

    addficaition

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,23 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h> // atoiを使用するため
-#include <string.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ryusukeyashiro <ryusukeyashiro@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/27 17:16:00 by ryusukeyash       #+#    #+#             */
+/*   Updated: 2024/04/27 17:16:45 by ryusukeyash      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	dst_len;
 	size_t	src_len;
-	size_t	max_copy_len;
 	size_t	i;
 
+	i = 0;
 	dst_len = strlen(dst);
 	src_len = strlen(src);
-	max_copy_len = dstsize > dst_len ? dstsize - dst_len - 1 : 0;
-	i = 0;
+	if (!dst || dstsize == 0)
+		return (dstsize + src_len);
 	if (dstsize <= dst_len)
 	{
 		return (dstsize + src_len);
 	}
-	while (i < max_copy_len && src[i] != '\0')
+	while (dst_len + i < dstsize - 1 && src[i])
 	{
 		dst[dst_len + i] = src[i];
 		i++;
@@ -25,22 +35,18 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	dst[dst_len + i] = '\0';
 	return (dst_len + src_len);
 }
-int	main(int argc, char **argv)
-{
-	size_t	result;
 
-	if (argc < 4)
-	{
-		printf("Usage: %s dst src size\n", argv[0]);
-		return (1);
-	}
-	char dst[256]; // 適切なバッファを確保
-	strncpy(dst, argv[1], sizeof(dst));
-	dst[sizeof(dst) - 1] = '\0'; // 安全のためにnull終端
-	size_t dstsize = atoi(argv[3]); // 文字列を数値に変換
-	result = ft_strlcat(dst, argv[2], dstsize);
-	size_t test = strlcat(dst, argv[2], dstsize); // strlcatを正しく使用
-	printf("%s, %zu\n", dst, result);
-	printf("%s, %zu\n", dst, test);
-	return (0);
-}
+// int	main(void)
+// {
+// 	char dst[256] = "";
+// 	const char *src = "42tokyo";
+// 	size_t dstsize = 0;
+// 	size_t result;
+
+// 	result = ft_strlcat(dst, src, dstsize);
+// 	printf("After ft_strlcat: \"%s\", Length: %zu\n", dst, result);
+
+// 	// result = strlcat(dst, src, dstsize);
+// 	// printf("After standard strlcat: \"%s\", Length: %zu\n", dst, result);
+// 	return (0);
+// }
