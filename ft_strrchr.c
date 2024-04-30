7f32cf72e2f96c75fe93b69003de5ea5c43c837b
ryusukeyashiro <ryusukeyashiro@ryusuke2024.iwatk1.kt.home.ne.jp>
2024-04-30 17:33:51 +0900

    strr remake

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ryusukeyashiro <ryusukeyashiro@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 11:37:25 by ryyashir          #+#    #+#             */
-/*   Updated: 2024/04/28 14:41:35 by ryusukeyash      ###   ########.fr       */
+/*   Updated: 2024/04/30 17:29:06 by ryusukeyash      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,51 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	const char	*hold;
+	char	*hold;
+	int		len;
 
-	hold = NULL;
-	while (*s)
+	len = 0;
+	hold = (char *)s;
+	while (*hold)
 	{
-		if (*s == c)
-			hold = s;
-		s++;
+		hold++;
+		len++;
 	}
 	if (c == '\0')
-		return ((char *)s);
-	return ((char *)hold);
-}
-/*
-int	main(int argc, char **argv)
-{
-		const char *str = argv[1];
-	int		c;
-	char	*result;
-	char	*test;
-
-	if(argc > 2)
+		return (hold);
+	hold--;
+	while (len)
 	{
-		c = argv[2][0];
-		result = ft_strrchr(str,c);
-		test = strrchr(str,c);
-		if (result) {
-			printf("Characterc '%c' found at poscition: %ld\n", c, result - str
-				+ 1);
-			printf("Characterc '%c' found at poscition: %ld\n", c, test - str
-				+ 1);
-		} else {
-			printf("Character '%c' not found in \"%s\"\n", c, str);
-			}
+		if (*hold == (char)c)
+			return (hold);
+		hold--;
+		len--;
 	}
 	return (0);
 }
-*/
+
+// #include <stdio.h>
+// #include <string.h>
+// int	main(int argc, char **argv)
+// {
+// 		const char *str = argv[1];
+// 	int		c;
+// 	char	*result;
+// 	char	*test;
+
+// 	if(argc > 2)
+// 	{
+// 		c = argv[2][0];
+// 		result = ft_strrchr(str,c);
+// 		test = strrchr(str,c);
+// 		if (result) {
+// 			printf("Characterc '%c' found at poscition: %ld\n", c, result - str
+// 				+ 1);
+// 			printf("Characterc '%c' found at poscition: %ld\n", c, test - str
+// 				+ 1);
+// 		} else {
+// 			printf("Character '%c' not found in \"%s\"\n", c, str);
+// 			}
+// 	}
+// 	return (0);
+// }
